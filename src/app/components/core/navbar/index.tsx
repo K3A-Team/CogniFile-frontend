99c70e48594629517560927ba2416e6eeb98562a
Abdelkarim17B <la_bengherbia@esi.dz>
2024-08-14 14:59:31 +0200

    style fixing

--- src/app/components/core/navbar/index.tsx
+++ src/app/components/core/navbar/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import dark from '@/public/dark.png';
@@ -5,24 +7,30 @@ import shortLogo from '@/public/logo_short.png';
 
 const Navbar = () => {
   return (
-    <nav className="py-4 px-8 border-b border-b-white">
+    <nav className="py-6 px-12 border-b border-b-[#787878]">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <Image src={shortLogo} alt="Logo" className="w-20" />
+          <Image src={shortLogo} alt="Logo" className="lg:w-20 w-16" />
         </div>
-        <div className="flex gap-x-4 items-center">
+        <div className="lg:flex hidden gap-x-8 items-center">
           <button>
-            <Image src={dark} alt="Dark" className="h-8 w-8" />
+            <Image src={dark} alt="Dark" className="h-12 w-12" />
           </button>
-          {/* <Link href="signin" className="text-white mr-4">
+          <Link href="/signin" className="text-white mr-4">
             Sign in
           </Link>
-          <Link href="/signup" className="text-white border border-white rounded-full px-4 py-2">
+          <Link
+            href="/signup"
+            className="text-white border border-white bg-cf-dark rounded-full px-4 py-2 hover:invert"
+          >
             Sign up - For free
-          </Link> */}
-          <Link href="/home" className="text-white border border-white rounded-full px-4 py-2">
-            Go Home
           </Link>
+        </div>
+
+        <div className="lg:hidden flex flex-col gap-2">
+          <div className="bg-white w-10 h-[2px] rounded-full"></div>
+          <div className="bg-white w-10 h-[2px] rounded-full"></div>
+          <div className="bg-white w-10 h-[2px] rounded-full"></div>
         </div>
       </div>
     </nav>
--- src/app/components/landing/hero/index.tsx
+++ src/app/components/landing/hero/index.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import shape5 from '@/public/Power.png';
+import shape6 from '@/public/arrow_right.png';
 import shape1 from '@/public/shape1.png';
 import shape2 from '@/public/shape2.png';
 import shape3 from '@/public/shape3.png';
@@ -6,34 +8,53 @@ import shape4 from '@/public/shape4.png';
 
 const Hero = () => {
   return (
-    <section className="py-20 relative">
-      <div className="w-full px-20 lg:px-60 text-center">
-        <div className="flex justify-center w-full">
-          <div className="flex items-center justify-between mb-8 px-2 border border-white rounded-full">
+    <section className="relative lg:scale-100 scale-60">
+      <div className="w-full px-20 text-center lg:px-40">
+        <div className="flex justify-center ">
+          <div className="lg:scale-100 scale-75 flex items-center justify-between mb-8 px-2 border border-white rounded-full">
+            <Image src={shape5} alt="Shape 5" className="h-6 w-auto" />
             <input
               type="text"
               placeholder="Find The Best Productivity software"
-              className="rounded-full px-6 py-3 focus:outline-none bg-transparent  w-96"
+              className="rounded-full px-2 py-3 focus:outline-none bg-transparent w-96"
             />
-            <button className="text-cf-dark bg-white rounded-full px-4 py-2">➔</button>
+            <button className="text-cf-dark bg-white rounded-full w-8 h-8 flex items-center justify-center">
+              <Image src={shape6} alt="Shape 6" className="h-3 w-auto" />
+            </button>
           </div>
         </div>
-        <h1 className="text-4xl md:text-6xl font-bold mb-6">
+        <h1 className="text-4xl lg:text-7xl font-extrabold mb-6 leading-normal lg:leading-normal text-center w-[100%]">
           Discover The Best Productivity Tool Effortlessly
         </h1>
-        <button className="bg-white text-cf-dark rounded-full px-16 py-4 font-bold text-lg">
+        <button className="lg:scale-100 scale-75 bg-gradient-to-r from-[#DEDEDE] to-[#787878] text-cf-dark rounded-full px-16 py-4 font-bold text-lg">
           Try For Free
         </button>
-        <p className="mt-6 text-lg">
+        <p className="mt-6 lg:text-xl leading-8 lg:w-auto max-w-[80%] m-auto">
           Over <span className="font-bold">200+ tools</span> and graphic design, Lorem ipsum is a
-          placeholder text <br />
+          placeholder text <br className="hidden lg:block" />
           commonly used to demonstrate the visual.
         </p>
       </div>
-      <Image src={shape1} alt="Shape 1" className="absolute top-10 left-10 w-10 h-10" />
-      <Image src={shape2} alt="Shape 2" className="absolute top-1/2 left-1/4 w-10 h-10" />
-      <Image src={shape3} alt="Shape 3" className="absolute top-10 right-10 w-10 h-10" />
-      <Image src={shape4} alt="Shape 4" className="absolute bottom-40 right-1/4 w-10 h-10" />
+      <Image
+        src={shape1}
+        alt="Shape 1"
+        className="hidden lg:block absolute top-10 left-10 lg:w-20 w-12 lg:h-20 h-12"
+      />
+      <Image
+        src={shape2}
+        alt="Shape 2"
+        className="hidden lg:block absolute top-1/2 left-[12%] lg:w-16 w-10 lg:h-16 h-10"
+      />
+      <Image
+        src={shape3}
+        alt="Shape 3"
+        className="hidden lg:block absolute top-10 right-10 lg:w-16 w-10 lg:h-16 h-10"
+      />
+      <Image
+        src={shape4}
+        alt="Shape 4"
+        className="hidden lg:block absolute bottom-32 right-[20%] lg:w-16 w-10 lg:h-16 h-10"
+      />
     </section>
   );
 };
