f36fbc58e9ec972ed06f7af77213a7214788eb3c
Abdelkarim17B <la_bengherbia@esi.dz>
2024-08-16 23:45:28 +0200

    Fixing Side bar, Home Page Added

---
+++ src/app/components/core/button/index.tsx
@@ -0,0 +1,24 @@
+interface buttonParams {
+  text: string;
+  icon: JSX.Element;
+  color: number;
+}
+
+function Button({ text, icon, color }: buttonParams) {
+  return (
+    <div
+      className={`p-[1px] ${color === 1 ? 'bg-Blue-gradient' : color === 2 ? 'bg-Orange-gradient' : 'bg-Red-gradient'} rounded-full flex items-center justify-center gap-2`}
+    >
+      <div className="bg-[#1f1f1f] flex items-center justify-center gap-2 px-4 py-2 rounded-full bg-clip-border">
+        {icon}
+        <span
+          className={`${color === 1 ? 'bg-Blue-gradient' : color === 2 ? 'bg-Orange-gradient' : 'bg-Red-gradient'} text-transparent bg-clip-text text-lg`}
+        >
+          {text}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Button;
