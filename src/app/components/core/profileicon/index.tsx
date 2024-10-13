499b8d9df4b6fc362ad3298668f03aa25c66b509
Zouambia Sohaib <js_zouambia@esi.dz>
2024-10-13 21:29:23 +0300

    added login toaster

--- src/app/components/core/profileicon/index.tsx
+++ src/app/components/core/profileicon/index.tsx
@@ -4,6 +4,7 @@ import MenuCard from '../menucard';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 
 // Adjust the import path as necessary
 
@@ -17,13 +18,26 @@ const ProfileIcon = () => {
   };
 
   const handleLogoutClick = () => {
-    // Handle logout logic here
+    setIsMenuVisible(false);
+    // Show a loading toast notification during the logout process
+    const toastId = toast.loading('Logging out...', {
+      position: 'bottom-right',
+      theme: 'dark', // Ensure dark mode for the toast
+    });
+
     axios
       .post('/api/auth/logout')
       .then(() => {
+        // Dismiss the loading toast after successful logout
+        toast.dismiss(toastId);
+
+        // Redirect to the login page
         router.push('/auth/login');
       })
       .catch(error => {
+        // Dismiss the toast on error
+        toast.dismiss(toastId);
+
         throw new Error(error);
       });
   };
@@ -51,20 +65,23 @@ const ProfileIcon = () => {
   }, []);
 
   return (
-    <div className="relative">
-      <button
-        className="rounded-full dark:bg-dar-card bg-white text-xl h-16 w-16 font-regular flex items-center justify-center dark:text-white text-[#AAAAAA]"
-        onClick={handleMenuToggle}
-      >
-        <p>{initials}</p>
-      </button>
+    <>
+      <div className="relative">
+        <button
+          className="rounded-full dark:bg-dar-card bg-white text-xl h-16 w-16 font-regular flex items-center justify-center dark:text-white text-[#AAAAAA]"
+          onClick={handleMenuToggle}
+        >
+          <p>{initials}</p>
+        </button>
 
-      {isMenuVisible && (
-        <div className="absolute top-2 -left-60">
-          <MenuCard items={menuItems} />
-        </div>
-      )}
-    </div>
+        {isMenuVisible && (
+          <div className="absolute top-2 -left-60">
+            <MenuCard items={menuItems} />
+          </div>
+        )}
+      </div>
+      <ToastContainer />
+    </>
   );
 };
 
